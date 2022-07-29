5f169dc7255e3540ceeeeb172a952a7574b6fabc
RohanYim <109933219+RohanYim@users.noreply.github.com>
2022-07-29 16:19:07 +0800

    Update autoreset.py

--- cogs/reset/autoreset.py
+++ cogs/reset/autoreset.py
@@ -12,7 +12,7 @@ def reset_mekaio():
     url = 'https://dashboard.mekrobotics.com/api/reset'
     payload = {}
     headers = {
-        'authorization': 'Bearer EyllEDF8P6XqP6jlvbEcikD4IFWVde',
+        'authorization': 'Bearer xxx',
         'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36',
     }
 
@@ -25,12 +25,12 @@ def reset_mekaio():
 
 
 def reset_stellar():
-    cookie_info = 'dashboard_session=s%3A-H6L0hdLojNJ4XpPYWJ24zoaZfmIgr6t.zpHVvXK1pJ0V3WGiqgY6dacwvUVtS9EolTO4WidxE8o'
+    cookie_info = 'dashboard_session=xxx'
     cookies = cookie_trans(cookie_info)
 
     url = 'https://account.stellara.io/api/reset'
     payload = {
-        'license_key': "6FBZH-7C47DWF9-L9QQL-FQ3UV"
+        'license_key': "xxx"
     }
     headers = {
         'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36',
@@ -44,7 +44,7 @@ def reset_stellar():
         return error
 
 def reset_kodai():
-    cookie_info = 'kodai_dashboard=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IllJTSM5Nzg0IiwiZW1haWwiOiJoZXJvbnNvbmcxMDI0QGdtYWlsLmNvbSIsImlkIjoiNTU5NDk1Njk4NzAyMTM5NDEyIiwiYXZhdGFyIjoiYmZhMGU1YWIwMzM2YTJjMjczOWY2ZGJmZDc5ODBiYTEiLCJkaXNjcmltaW5hdG9yIjoiOTc4NCIsInN0b3JhZ2UiOiIxZmZhOTFhYjhkZjQyNDEwYzg1YmJmZWY3YjMzMWNjODQ4YjY4NDllYzYwYjI0NzZmNTI2ZGZiZDM3Y2FmZTdkNWVhZGJmOWJlMzUxODg0ZmNhNTNjNmEyOTQzYTJlMWJhODViMTJhM2YzYmVkMDJmZTY3N2FmMTNjM2UyZjI2Zjk4ZTk5NzFmZDU0NjBmZDRkNzM1YWRkNzQxMzRjY2VmNmZhNWVhNTFkYTIzOTM0YjIzNjFmMTIzNDFmNDVjZjI2Yzg3YWY3YjE3N2ZlODBhYjFlNzNlMjc4ODgyODg1MDZjZDdlYzdlYzIxMmUwYWMwNTJmZGEyNDcwZmM1ZGZlNDliYTUzYWZkMjM2MDE3MjM1ZTI2OWMxOTA4NDViMDExNmQzZjIxYjY2YzkyZWVkMTcwYjEyNGFhZmM4MzFkOTgzMTA2ZTY1ZWQ5YzNhNTYzNjgxOGRlNjQxZGI5Y2FlYmRjNjc5YTk5MmIzYzQzMTllN2RjOTFjODQwYTc5ZWRhNzFiMjZiMzc4MmE1Y2FiMTU0ZGQzNjcxOGJiYzZhZCIsImlwIjoiMjE2LjEyNi4yMjQuMTAyIiwiaWF0IjoxNjI5MDAxODA3fQ.REGfy2rkxDnlfm4yxqq-icn4Db6t0_9iHN80XQCVqzw'
+    cookie_info = 'kodai_dashboard=xxx'
     cookies = cookie_trans(cookie_info)
 
     url = 'https://hub.kodai.io/api/user/unbind'
@@ -65,10 +65,10 @@ def reset_kodai():
 def reset_koi():
     url = 'https://dash.koi.solutions/api/user/reset/machine'
     payload = {
-        'licenseKey': "FYANWP-HXATLA-NDNHVE-TKCQDL",
+        'licenseKey': "xxx",
     }
     headers = {
-        'authorization': 'Bearer iJO2eZbd0xFII4kyDEvqM7YJ4EOpVi',
+        'authorization': 'Bearer xxx',
         'content-type': 'application/json',
         'tl_client': 'koisolutions',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
